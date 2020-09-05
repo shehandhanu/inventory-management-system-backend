c677d083a2f5e6b36ae923ab41287ca382439132
Shahan Dhanuddara <65756207+shehandhanu@users.noreply.github.com>
2020-09-05 12:32:15 +0530

    get All Products

--- controllers/product.Controller.js
+++ controllers/product.Controller.js
@@ -11,3 +11,14 @@ exports.newProducts = async (req, res, next) => {
     })
 
 }
+//get all products  =>/api/v1/products?keyword=rose
+exports.getProducts = async (req, res, next) => {
+
+    const products = await Product.find();
+
+    res.status(200).json({
+        success: true,
+        count: products.length,
+        products
+    })
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const { newProducts, } = require('../controllers/product.Controller');
+const { newProducts,
+    getProducts } = require('../controllers/product.Controller');
 
 router.route('/product').post(newProducts);
+router.route('/product').get(getProducts);
 
 module.exports = router;
