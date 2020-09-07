c13979437b5d866a6bff67cbb7e2d5b3b8362ff5
Shahan Dhanuddara <65756207+shehandhanu@users.noreply.github.com>
2020-09-07 13:03:25 +0530

    product Update Works

--- controllers/product.Controller.js
+++ controllers/product.Controller.js
@@ -37,3 +37,23 @@ exports.getSingleProduct = async (req, res, next) => {
         product
     })
 }
+
+//Update Product => /api/v1/product/:id 
+exports.updateProduct = async (req, res, next) => {
+
+    let product = await Product.findById(req.params.id)
+    if (!product) {
+        return next(new ErrorHandler('Product Not Found', 404))
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,12 +4,16 @@ const router = express.Router();
 
 const { newProducts,
     getProducts,
-    getSingleProduct } = require('../controllers/product.Controller');
+    getSingleProduct,
+    updateProduct } = require('../controllers/product.Controller');
 //Add Product
 router.route('/product').post(newProducts);
 //Get All Products
 router.route('/product').get(getProducts);
 //Get One Product
 router.route('/product/:id').get(getSingleProduct);
+//Update Products
+router.route('/product/:id').put(updateProduct);
+
 
 module.exports = router;
